fad922ba63bb7bcea5b5dc35b4538a12fd2dc249
porucado <e.econop@gmail.com>
2019-09-29 19:17:58 +0900

    modify "binary.py "

--- binary.py
+++ binary.py
@@ -5,7 +5,7 @@ img = cv2.imread(filename, 0) # 画像の読み込み
 
 threshold = 100 # 閾値の設定
 
-ret, img_thresh = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY) # 二値化(閾値100を超えた画素を255にする。)
+ret, img_thresh = cv2.threshold(img, threshold, 255, cv2.THRESH_OTSU) # 二値化(閾値100を超えた画素を255にする。)
 
 # 二値化画像の表示
 #cv2.imwrite(r'C:\Users\eecon\Desktop\img_binary', img_thresh)
